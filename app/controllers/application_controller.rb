271b393d4f6e47f3aabd912a84b9585a63db2cbf
Reice Griffin <reicelovesllamas@gmail.com>
2018-08-01 16:45:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,19 @@ class ApplicationController < Sinatra::Base
 
   get '/'do
   "BYE FOELESHA"
-end 
-
+end  
+  get '/volunteer'do    
+    "Hello"
 end
+ 
+ get '/donate'do
+"hello"
+end
+
+get '/ADHD'
+"hello"
+end 
+end 
+# end for the class App 
+
+
